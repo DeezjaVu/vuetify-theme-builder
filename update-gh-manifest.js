2c1d590be52a44cc3019ae9a5ca7073a0b4ff57b
DeezjaVu <p.ginneberge@gmail.com>
2024-12-19 14:29:35 +0100

    - Added manifest commands to package.json - Modified github workflow to update manifest.

---
+++ update-gh-manifest.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const path = require("path");
+
+const isGitHubPages = process.env.GITHUB_PAGES === "true";
+const repoName = "vuetify-theme-builder";
+const manifestPath = path.join(__dirname, "public", "manifest.json");
+
+fs.readFile(manifestPath, "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading manifest file:", err);
+    return;
+  }
+
+  const manifest = JSON.parse(data);
+  if (isGitHubPages) {
+    manifest.icons.forEach((icon) => {
+      icon.src = `/${repoName}${icon.src}`;
+    });
+  }
+
+  fs.writeFile(manifestPath, JSON.stringify(manifest, null, 2), "utf8", (err) => {
+    if (err) {
+      console.error("Error writing manifest file:", err);
+    } else {
+      console.log("Manifest file updated successfully.");
+    }
+  });
+});
